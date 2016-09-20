169046f318220fad40f5dff2167355df80230d36
Patrick Gao <patrickgao0922@gmail.com>
2016-09-20 16:51:11 +1000

    Initial Commit

---
+++ TableViewCellImageAsynLoading/TableViewCellImageAsynLoading.h
@@ -0,0 +1,19 @@
+//
+//  TableViewCellImageAsynLoading.h
+//  TableViewCellImageAsynLoading
+//
+//  Created by Work on 20/09/2016.
+//  Copyright © 2016 com.patrickgao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TableViewCellImageAsynLoading.
+FOUNDATION_EXPORT double TableViewCellImageAsynLoadingVersionNumber;
+
+//! Project version string for TableViewCellImageAsynLoading.
+FOUNDATION_EXPORT const unsigned char TableViewCellImageAsynLoadingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TableViewCellImageAsynLoading/PublicHeader.h>
+
+
